cc3fad52ac789d6a0aaf2846e5ff13d3e8d583f8
osukaru16 <osukaru16@gmail.com>
2017-01-26 12:47:04 +0100

    Add files via upload

--- KataBisiesto/src/Bisiesto/AnyBisiesto.java
+++ KataBisiesto/src/Bisiesto/AnyBisiesto.java
@@ -1,27 +1,42 @@
 package Bisiesto;
 
 public class AnyBisiesto {
-	private int any;
+	private Integer any;
 
 	public AnyBisiesto() {
 		this.any = 1904;
 	}
 	
-	public AnyBisiesto(int any) {
+	public AnyBisiesto(Integer any) {
 		this.any = any;
 	}
 	
 	
-	public int getAny(){
+	public Integer getAny(){
 		return this.any;
 	}
 	
 	
 	
 	
+	public void setAny(Integer any){
+		this.any = any;
+	}
+	
+	
+	
+	
+	
 	
 	public Boolean esBisiesto(){
-		return getAny() % 4 == 0;
+		String comprobador  = getAny().toString();
+		Boolean sentencia = comprobador.substring(comprobador.length()-2, comprobador.length()).equals("00");
+		if((sentencia)&&(getAny()>=400)){
+			return getAny() % 400 == 0;
+		}
+		else{
+		return  getAny() % 4 == 0;
+		}
 	}
 
 }
--- KataBisiesto/src/Bisiesto/AnyBisiestoTest.java
+++ KataBisiesto/src/Bisiesto/AnyBisiestoTest.java
@@ -27,6 +27,54 @@ public class AnyBisiestoTest {
 		assertEquals("Es el 1999",false, any.esBisiesto());
 	}
 	
+	@Test
+	public void test_secular_bisiesto_2000() {
+		AnyBisiesto any = new AnyBisiesto(2000);
+		assertEquals("Es el 2000",true, any.esBisiesto());
+	}
+	
+	@Test
+	public void test_secular_2100() {
+		AnyBisiesto any = new AnyBisiesto(2100);
+		assertEquals("Es el 2100",false, any.esBisiesto());
+	}
+	
+	@Test
+	public void test_secular_set() {
+		AnyBisiesto any = new AnyBisiesto(2100);
+		assertEquals("Es el 2100",false, any.esBisiesto());
+		any.setAny(2000);
+		assertEquals("Es el 2000",true, any.esBisiesto());
+		
+	}
+	
+	
+	
+	@Test
+	public void test_204() {
+		AnyBisiesto any = new AnyBisiesto(204);
+		assertEquals("Es el 204",true, any.esBisiesto());
+	}
+	
+	
+	
+	
+	@Test
+	public void test_200() {
+		AnyBisiesto any = new AnyBisiesto(200);
+		assertEquals("Es el 200",true, any.esBisiesto());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
